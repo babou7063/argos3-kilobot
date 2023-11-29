d52a27204865cc517f0d1cca5ab4620ba08c26d2
babou7063 <babette.roland1@gmail.com>
2023-11-29 14:32:13 +0100

    aggregation.c 2.0.0

--- src/examples/behaviors/aggregation.c
+++ src/examples/behaviors/aggregation.c
@@ -1,33 +1,62 @@
 #include <kilolib.h>
 
-void setup(){
+// Initalisation des variables globales
+message_t send_msg;
+message_t rcv_msg;
+int new_message = 0;
+int current_state = 1;
+int previous_state = 1;
 
+
+void setup(){
+    // Assigniation de la valeur du message et calcule de son CRC
+    send_msg.type = NORMAL;
+    send_msg.data[0] = 1; // comment récupérer la valeur qui se trouve dans le fichier XML
+    send_msg.crc = message_crc(&send_msg);
 }
 
 void loop(){
-    /* Envoie du message durant rcv_timing ms */
+    
+    // Action de random walk ssi on a reçut un nouveau message
+    if (new_message == 1){
+        
+        // Changer le current_state en fct des mess reçut
+        SetStateSurrondingRobots(rcv_msg);
 
+        // encore une fois il faut y associer les états ici 
+        switch (current_state)
+        {
+        case 0:
+            
+            break;
+        
+        case 1:
+            
+            break;
 
-    /* Reception des messages */
+        case 2:
+        
+            break;
 
+        case 3:
+            
+            break;
 
-    /* Change l'état en fct des mess reçut */
+        // gestion erreur 
+        default:
 
-
-    /* Action de random walk */
-
+            break;
+        }
+    }
+    else {
+        // si pas de message on continue comme avant
+        current_state = previous_state;
+    }
 
 }
 
-void send_message(){
 
-}
-
-void recieve_message(){
-
-}
-
-void SetStateSurrondingRobots(){
+void SetStateSurrondingRobots(message_t msg){
     
 }
 
@@ -47,8 +76,23 @@ RW_NFE(){
     
 }
 
-int  main(){
+message_t *message_tx(){
+    return &send_msg;
+}
+
+void message_rx (message_t *message, distance_measurement_t *distance){
+    // on stocke le message qu'on recoit
+    rcv_msg = *message;
+    // on met a jour le bool
+    new_message = 1;
+}
+
+int main(){
     kilo_init();
+
+    kilo_message_tx = message_tx;
+    kilo_message_rx = message_rx;
+
     kilo_start(setup, loop);
 
     return 0;
