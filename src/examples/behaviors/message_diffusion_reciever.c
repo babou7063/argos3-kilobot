f44c6f26f7e2871ff9943c587083f64cb0b255dd
babou7063 <babette.roland1@gmail.com>
2023-12-06 19:56:10 +0100

    message_diffusion 3.0.0

--- src/examples/behaviors/message_diffusion_reciever.c
+++ src/examples/behaviors/message_diffusion_reciever.c
@@ -1,13 +1,47 @@
 #include <kilolib.h>
 
-void setup(){
+uint8_t recvd_message;
+uint8_t message_sent;
+message_t msg;
+message_t recvd_msg;
 
+message_t *message_tx(){
+    return &recvd_msg;
+}
+
+void rx_message(message_t *msg, distance_measurement_t *d){
+    recvd_msg = *msg;
+    recvd_message = 1;
+}
+
+void setup(){
+    recvd_message = 0;
+
+    msg.type = NORMAL;
+    msg.data[0] = recvd_msg.data[0];
+    msg.data[1] = recvd_msg.data[1];
+    msg.data[2]  = recvd_msg.data[2];
+    msg.crc = message_crc(&msg);
 }
 
 void loop(){
+    if(recvd_message){
+        recvd_message = 0;
 
+        // on fait clignoter la led de la couleur du message
+        set_color(RGB(recvd_msg.data[0], recvd_msg.data[1], recvd_msg.data[2]));
+        delay(100);
+        set_color(RGB(0, 0, 0));
+    }
 }
 
 int main(){
+    kilo_init();
+
+    kilo_message_rx = rx_message;
+    kilo_message_tx = message_tx;
+
+    kilo_start(setup, loop);
+
     return 0;
 }
--- src/examples/behaviors/message_diffusion_sender.c
+++ src/examples/behaviors/message_diffusion_sender.c
@@ -1,13 +1,41 @@
 #include <kilolib.h>
 
-void setup(){
+// Initialisation variable globale
+message_t msg;
+uint8_t message_sent;
 
+message_t *message_tx(){
+    return &msg;
+}
+
+void tx_message_success(){
+    message_sent = 1;
+}
+
+void setup(){
+    msg.type = NORMAL;
+    msg.data[0] = 1;
+    msg.data[1] = 1;
+    msg.data[2]  = 1;
+    msg.crc = message_crc(&msg);
 }
 
 void loop(){
-
+    if (message_sent){
+        message_sent = 0;
+        set_color(RGB(1, 0, 0));
+        delay(100);
+        set_color(RGB(0, 0, 0));
+    }
 }
 
 int main(){
+    kilo_init();
+
+    kilo_message_tx = message_tx;
+    kilo_message_tx_success = tx_message_success;
+
+    kilo_start(setup, loop);
+
     return 0;
 }
